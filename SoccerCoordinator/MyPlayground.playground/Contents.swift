8ecbb7f2001808a22673a875be377e1fdf08be3c
TomBastable <bastable@gmail.com>
2018-02-26 22:36:51 +0000

    Added Player Collections
    Added the player collections and each of their individual player data.

--- SoccerCoordinator/MyPlayground.playground/Contents.swift
+++ SoccerCoordinator/MyPlayground.playground/Contents.swift
@@ -1,8 +1,127 @@
-//: Playground - noun: a place where people can play
+// Create an individual dictionary for each player
 
-import UIKit
+var player1: [String: Any] = [
+    "Name": "Joe Smith",
+    "Height": 42,
+    "Soccer Experience": true,
+    "Guardian Name": "Jim and Jan Smith",
+]
 
-var str = "Hello, playground"
+var player2: [String: Any] = [
+    "Name": "Jill Tanner",
+    "Height": 36,
+    "Soccer Experience": true,
+    "Guardian Name": "Clara Tanner",
+]
 
-//Tom Note
+var player3: [String: Any] = [
+    "Name": "Bill Bon",
+    "Height": 43,
+    "Soccer Experience": true,
+    "Guardian Name": "Sara and Jenny Bon",
+]
 
+var player4: [String: Any] = [
+    "Name": "Eva Gordon",
+    "Height": 45,
+    "Soccer Experience": false,
+    "Guardian Name": "Wendy and Mike Gordon",
+]
+
+var player5: [String: Any] = [
+    "Name": "Matt Gill",
+    "Height": 40,
+    "Soccer Experience": false,
+    "Guardian Name": "Charles and Sylvia Gill",
+]
+
+var player6: [String: Any] = [
+    "Name": "Kimmy Stein",
+    "Height": 41,
+    "Soccer Experience": false,
+    "Guardian Name": "Bill and Hillary Stein",
+]
+
+var player7: [String: Any] = [
+    "Name": "Sammy Adams",
+    "Height": 45,
+    "Soccer Experience": false,
+    "Guardian Name": "Jeff Adams",
+]
+
+var player8: [String: Any] = [
+    "Name": "Karl Saygan",
+    "Height": 42,
+    "Soccer Experience": true,
+    "Guardian Name": "Heather Bledsoe",
+]
+
+var player9: [String: Any] = [
+    "Name": "Suzane Greenberg",
+    "Height": 44,
+    "Soccer Experience": true,
+    "Guardian Name": "Henrietta Dumas",
+]
+
+var player10: [String: Any] = [
+    "Name": "Sal Dali",
+    "Height": 41,
+    "Soccer Experience": false,
+    "Guardian Name": "Gala Dali",
+]
+
+var player11: [String: Any] = [
+    "Name": "Joe Kavalier",
+    "Height": 39,
+    "Soccer Experience": false,
+    "Guardian Name": "Sam and Elaine Kavalier",
+]
+
+var player12: [String: Any] = [
+    "Name": "Ben Finkelstein",
+    "Height": 44,
+    "Soccer Experience": false,
+    "Guardian Name": "Aaron and Jill Finkelstein",
+]
+
+var player13: [String: Any] = [
+    "Name": "Diego Soto",
+    "Height": 41,
+    "Soccer Experience": true,
+    "Guardian Name": "Robin and Sarika Soto",
+]
+
+var player14: [String: Any] = [
+    "Name": "Chloe Alaska",
+    "Height": 47,
+    "Soccer Experience": false,
+    "Guardian Name": "David and Jamie Alaska",
+]
+
+var player15: [String: Any] = [
+    "Name": "Arnold Willis",
+    "Height": 43,
+    "Soccer Experience": false,
+    "Guardian Name": "Claire Willis",
+]
+
+var player16: [String: Any] = [
+    "Name": "Phillip Helm",
+    "Height": 44,
+    "Soccer Experience": true,
+    "Guardian Name": "Thomas Helm and Eva Jones",
+]
+
+var player17: [String: Any] = [
+    "Name": "Les Clay",
+    "Height": 42,
+    "Soccer Experience": true,
+    "Guardian Name": "Wynonna Brown",
+]
+
+var player18: [String: Any] = [
+    "Name": "Herschel Krustofski",
+    "Height": 45,
+    "Soccer Experience": true,
+    "Guardian Name": "Hyman and Rachel Krustofski",
+]
